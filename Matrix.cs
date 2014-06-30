f096bd6607fcb6ba0d533c868aa20a96db556b65
dponce_85 <dponce_85@hotmail.com>
2014-06-30 10:06:34 -0500

    Removed Matrix Class

--- Matrix.cs
+++ Matrix.cs
@@ -8,28 +8,46 @@ namespace GuiSonar2
     class Matrix
     {
         private double[] _Data;
-        private int _Rows;
-        private int _Cols;
+        private int[] _Size;
+        private int _Numel;
 
         public Matrix(Matrix size)
         {
-            _Rows = (int)size[0];
-            _Cols = (int)size[1];
-            _Data = new double[_Rows * _Cols];
+            init(size);
         }
 
-        public Matrix(int rows, int cols)
+        public Matrix(params int[] size)
         {
-            _Rows = rows;
-            _Cols = cols;
-            _Data = new double[_Rows * _Cols];
+            init(size);
+        }
+
+        private void init(int[] size)
+        {
+            if (size.Length == 0)
+                throw new ArgumentException("Invalid number of dimensions");
+
+            _Size = size;
+            _Numel = getNumel(size);
+            _Data = new double[_Numel];
+        }
+
+        private int getNumel(int[] size)
+        {
+            int numel = 1;
+            for (int i = 0; i < size.Length; i++)
+                numel *= size[i];
+
+            if (numel < 0)
+                throw new ArgumentException("Size must be an array of positive numbers");
+
+            return numel;
         }
 
         public int Length
         { 
             get
             {
-                return (_Rows > _Cols) ? _Rows : _Cols;
+                return _Size.Max();
             }            
         }
 
@@ -37,184 +55,344 @@ namespace GuiSonar2
         {
             get
             {
-                return _Rows * _Cols;
+                return _Numel;
             }
         }
-        
+
+        public int Ndims
+        {
+            get 
+            {
+                return _Size.Length;
+            }
+        }
+
+
+        private static bool IsScalar(Matrix x)
+        {
+            // Check for scalars
+            return (x.Numel == 1);                
+        }
+
+        private static bool CheckDims(Matrix x, Matrix y)
+        {
+            // Check dims
+            if (x.Ndims != y.Ndims)
+                return false;
+
+            // Check size
+            Matrix dS = (x.Size.Equal(y.Size));
+            if (dS.Sum != x.Ndims)
+                return false;
+
+            return true;
+        }
+ 
+
+
+
+
+
+
+
+        public void Add(Matrix y)
+        {
+            // Check for scalars
+            if (IsScalar(y))
+            {
+                double y0 = y[0];
+                for (int i = 0; i < this.Numel; i++)
+                    this[i] += y0;
+            }
+            else
+            {
+                // Check for identical dimensions and sizes
+                if (!CheckDims(this, y))
+                    throw new ArgumentException("Error using  Add \n Matrix dimensions must agree");
+
+                // Cleared!
+                for (int i = 0; i < this.Numel; i++)
+                    this[i] += y[i];
+            }
+        }
+
         public static Matrix operator +(Matrix x, Matrix y)
         {
-            if (y.Length == 1)
-                for (int i = 0; i < x.Length; i++)
-                    x[i] += y[0];
+            Matrix z;
+            if (IsScalar(y))
+            { 
+                z = x.Clone();
+            }
+            else if (IsScalar(x))
+            {
+                z = y.Clone();
+            }
+            else
+            {
+
+            }
+
             
-            else if (y.Length == x.Length)
-                for (int i = 0; i < x.Length; i++)
-                    x[i] += y[i];
-
-            else
-                throw new ArgumentException("Error using  + \n Matrix dimensions must agree");
-
-            return x;
+            z.Add(y);
+            return z;
         }
 
-        public static Matrix operator -(Matrix x, Matrix y)
+
+
+
+
+
+
+
+        public void Substract(Matrix y)
         {
-            if (y.Length == 1)
-                for (int i = 0; i < x.Length; i++)
-                    x[i] -= y[0];
-
-            else if (y.Length == x.Length)
-                for (int i = 0; i < x.Length; i++)
-                    x[i] -= y[i];
-
+            // Check for scalars
+            if (IsScalar(y))
+            {
+                double y0 = y[0];
+                for (int i = 0; i < this.Numel; i++)
+                    this[i] -= y0;
+            }
             else
-                throw new ArgumentException("Error using  - \n Matrix dimensions must agree");
+            {
+                // Check for identical dimensions and sizes
+                if (!CheckDims(this, y))
+                    throw new ArgumentException("Error using  Substract \n Matrix dimensions must agree");
 
-            return x;
+                // Cleared!
+                for (int i = 0; i < this.Numel; i++)
+                    this[i] -= y[i];
+            }
         }
 
 
-        public static Matrix operator *(Matrix x, Matrix y)
+
+
+       
+
+
+        public void Product(Matrix y)
         {
-            if (y.Length == 1)
-                for (int i = 0; i < x.Length; i++)
-                    x[i] *= y[0];
-
-            else if (y.Length == x.Length)
-                for (int i = 0; i < x.Length; i++)
-                    x[i] *= y[i];
-
+            // Check for scalars
+            if (IsScalar(y))
+            {
+                double y0 = y[0];
+                for (int i = 0; i < this.Numel; i++)
+                    this[i] *= y0;
+            }
             else
-                throw new ArgumentException("Error using  * \n Matrix dimensions must agree");
+            {
+                // Check for identical dimensions and sizes
+                if (!CheckDims(this, y))
+                    throw new ArgumentException("Error using  Product \n Matrix dimensions must agree");
 
-            return x;
+                // Cleared!
+                for (int i = 0; i < this.Numel; i++)
+                    this[i] *= y[i];
+            }
         }
 
 
-        public static Matrix operator /(Matrix x, Matrix y)
+
+
+
+
+        public void Divide(Matrix y)
         {
-            if (y.Length == 1)
-                for (int i = 0; i < x.Length; i++)
-                    x[i] /= y[0];
-
-            else if (y.Length == x.Length)
-                for (int i = 0; i < x.Length; i++)
-                    x[i] /= y[i];
-
+            // Check for scalars
+            if (IsScalar(y))
+            {
+                double y0 = y[0];
+                for (int i = 0; i < this.Numel; i++)
+                    this[i] /= y0;
+            }
             else
-                throw new ArgumentException("Error using  / \n Matrix dimensions must agree");
+            {
+                // Check for identical dimensions and sizes
+                if (!CheckDims(this, y))
+                    throw new ArgumentException("Error using  Divide \n Matrix dimensions must agree");
 
-            return x;
+                // Cleared!
+                for (int i = 0; i < this.Numel; i++)
+                    this[i] /= y[i];
+            }
         }
 
-        public static Matrix operator >(Matrix x, Matrix y)
+
+
+
+
+
+        public void GreaterThan(Matrix y)
         {
-            Matrix z = new Matrix(x.Size);
-
-            if (x.Length == 1)
-                for (int i = 0; i < x.Length; i++)
-                    z[i] = (x[i] > y[0]) ? 1 : 0;
-
-            else if (y.Length == x.Length)
-                for (int i = 0; i < x.Length; i++)
-                    z[i] = (x[i] > y[i]) ? 1 : 0;
-
+            // Check for scalars
+            if (IsScalar(y))
+            {
+                double y0 = y[0];
+                for (int i = 0; i < this.Numel; i++)
+                    this[i] = (this[i] > y0) ? 1 : 0;
+            }
             else
-                throw new ArgumentException("Error using  > \n Matrix dimensions must agree");
+            {
+                // Check for identical dimensions and sizes
+                if (!CheckDims(this, y))
+                    throw new ArgumentException("Error using  GreaterThan \n Matrix dimensions must agree");
 
-            return x;
+                // Cleared!
+                for (int i = 0; i < this.Numel; i++)
+                    this[i] = (this[i] > y[i]) ? 1 : 0;
+            }
         }
 
-        public static Matrix operator >=(Matrix x, Matrix y)
+
+
+
+
+
+        public void GreaterEqualThan(Matrix y)
         {
-            Matrix z = new Matrix(x.Size);
-
-            if (x.Length == 1)
-                for (int i = 0; i < x.Length; i++)
-                    z[i] = (x[i] >= y[0]) ? 1 : 0;
-
-            else if (y.Length == x.Length)
-                for (int i = 0; i < x.Length; i++)
-                    z[i] = (x[i] >= y[i]) ? 1 : 0;
-
+            // Check for scalars
+            if (IsScalar(y))
+            {
+                double y0 = y[0];
+                for (int i = 0; i < this.Numel; i++)
+                    this[i] = (this[i] >= y0) ? 1 : 0;
+            }
             else
-                throw new ArgumentException("Error using  > \n Matrix dimensions must agree");
+            {
+                // Check for identical dimensions and sizes
+                if (!CheckDims(this, y))
+                    throw new ArgumentException("Error using  GreaterEqualThan \n Matrix dimensions must agree");
 
-            return x;
+                // Cleared!
+                for (int i = 0; i < this.Numel; i++)
+                    this[i] = (this[i] >= y[i]) ? 1 : 0;
+            }
         }
 
 
-        public static Matrix operator <(Matrix x, Matrix y)
+
+
+
+
+
+        public void LessThan(Matrix y)
         {
-            Matrix z = new Matrix(x.Size);
-
-            if (x.Length == 1)
-                for (int i = 0; i < x.Length; i++)
-                    z[i] = (x[i] < y[0]) ? 1 : 0;
-
-            else if (y.Length == x.Length)
-                for (int i = 0; i < x.Length; i++)
-                    z[i] = (x[i] < y[i]) ? 1 : 0;
-
+            // Check for scalars
+            if (IsScalar(y))
+            {
+                double y0 = y[0];
+                for (int i = 0; i < this.Numel; i++)
+                    this[i] = (this[i] < y0) ? 1 : 0;
+            }
             else
-                throw new ArgumentException("Error using  > \n Matrix dimensions must agree");
+            {
+                // Check for identical dimensions and sizes
+                if (!CheckDims(this, y))
+                    throw new ArgumentException("Error using  LessThan \n Matrix dimensions must agree");
 
-            return x;
+                // Cleared!
+                for (int i = 0; i < this.Numel; i++)
+                    this[i] = (this[i] < y[i]) ? 1 : 0;
+            }
         }
 
-        public static Matrix operator <=(Matrix x, Matrix y)
+
+
+
+        public void LessEqualThan(Matrix y)
         {
-            Matrix z = new Matrix(x.Size);
-
-            if (x.Length == 1)
-                for (int i = 0; i < x.Length; i++)
-                    z[i] = (x[i] <= y[0]) ? 1 : 0;
-
-            else if (y.Length == x.Length)
-                for (int i = 0; i < x.Length; i++)
-                    z[i] = (x[i] <= y[i]) ? 1 : 0;
-
+            // Check for scalars
+            if (IsScalar(y))
+            {
+                double y0 = y[0];
+                for (int i = 0; i < this.Numel; i++)
+                    this[i] = (this[i] <= y0) ? 1 : 0;
+            }
             else
-                throw new ArgumentException("Error using  > \n Matrix dimensions must agree");
+            {
+                // Check for identical dimensions and sizes
+                if (!CheckDims(this, y))
+                    throw new ArgumentException("Error using  LessEqualThan \n Matrix dimensions must agree");
 
-            return x;
+                // Cleared!
+                for (int i = 0; i < this.Numel; i++)
+                    this[i] = (this[i] <= y[i]) ? 1 : 0;
+            }
         }
 
-        public static Matrix operator ==(Matrix x, Matrix y)
+
+
+
+
+        public void Equal(Matrix y)
         {
-            Matrix z = new Matrix(x.Size);
-
-            if (x.Length == 1)
-                for (int i = 0; i < x.Length; i++)
-                    z[i] = (x[i] == y[0]) ? 1 : 0;
-
-            else if (y.Length == x.Length)
-                for (int i = 0; i < x.Length; i++)
-                    z[i] = (x[i] == y[i]) ? 1 : 0;
-
+            // Check for scalars
+            if (IsScalar(y))
+            {
+                double y0 = y[0];
+                for (int i = 0; i < this.Numel; i++)
+                    this[i] = (this[i] == y0) ? 1 : 0;
+            }
             else
-                throw new ArgumentException("Error using  > \n Matrix dimensions must agree");
+            {
+                // Check for identical dimensions and sizes
+                if (!CheckDims(this, y))
+                    throw new ArgumentException("Error using  LessEqualThan \n Matrix dimensions must agree");
 
-            return x;
+                // Cleared!
+                for (int i = 0; i < this.Numel; i++)
+                    this[i] = (this[i] == y[i]) ? 1 : 0;
+            }
         }
 
-        public static Matrix operator !=(Matrix x, Matrix y)
+
+
+
+
+        public void NotEqual(Matrix y)
         {
-            Matrix z = new Matrix(x.Size);
-
-            if (x.Length == 1)
-                for (int i = 0; i < x.Length; i++)
-                    z[i] = (x[i] != y[0]) ? 1 : 0;
-
-            else if (y.Length == x.Length)
-                for (int i = 0; i < x.Length; i++)
-                    z[i] = (x[i] != y[i]) ? 1 : 0;
-
+            // Check for scalars
+            if (IsScalar(y))
+            {
+                double y0 = y[0];
+                for (int i = 0; i < this.Numel; i++)
+                    this[i] = (this[i] != y0) ? 1 : 0;
+            }
             else
-                throw new ArgumentException("Error using  > \n Matrix dimensions must agree");
+            {
+                // Check for identical dimensions and sizes
+                if (!CheckDims(this, y))
+                    throw new ArgumentException("Error using  LessEqualThan \n Matrix dimensions must agree");
 
-            return x;
+                // Cleared!
+                for (int i = 0; i < this.Numel; i++)
+                    this[i] = (this[i] != y[i]) ? 1 : 0;
+            }
         }
+
+
+
+        public double Sum
+        {
+            get
+            {
+                double ret = 0;
+                for (int i = 0; i < this.Numel; i++)
+                    ret += this[i];
+
+                return ret;
+            }
+        }
+
+        public double Mean
+        {
+            get
+            {
+                return this.Sum / this.Numel;
+            }
+        }
+
 
 
         public Matrix this[Matrix idx]
@@ -263,37 +441,74 @@ namespace GuiSonar2
             }
         }
 
-        public double this[int row, int col]
+        public double this[params int[] args]
         {
             get
             {
-                if ((row < _Rows) && (row >= 0))
-                    if ((col < _Cols) && (col >= 0))
-                        return _Data[row * col];
-                    else
+                for (int i = 0; i < args.Length; i++)
+                    if (args[i] > this.Size[i])                        
                         throw new ArgumentException("Index exceeds matrix dimensions.");
-                else throw new ArgumentException("Index exceeds matrix dimensions.");
+
+                return _Data[indexToAddress(args)];                    
             }
             set
             {
-                if ((row < _Rows) && (row >= 0))
-                    if ((col < _Cols) && (col >= 0))
-                        _Data[row * col] = value;
-                    else
+                for (int i = 0; i < args.Length; i++)
+                    if (args[i] > this.Size[i])                        
                         throw new ArgumentException("Index exceeds matrix dimensions.");
-                else throw new ArgumentException("Index exceeds matrix dimensions.");
+
+                _Data[indexToAddress(args)] = value;
             }
+        }
+
+        private int indexToAddress(int[] index)
+        {
+            int address = 0;            
+            for (int i = 0; i < index.Length; i++)
+            {
+                int prod = 1;
+                for (int j = 0; j < i - 1; j++)
+                    prod = prod * this._Size[j];
+
+                address += index[i] * prod;
+            }
+
+            return address;
+        }
+
+        private int cumsum(int[] arg)
+        { 
+            int sum = 0;
+            for (int i = 0; i < arg.Length; i++)
+                sum += arg[i];
+
+            return sum;
+        }
+
+        private int cumprod(int[] arg)
+        {
+            int prod = arg[0];
+            for (int i = 1; i < arg.Length; i++)
+                prod *= arg[i];
+
+            return prod;
         }
 
         public Matrix Size
         {   
             get
-            {
-                Matrix s = new Matrix(1, 2);
-                s[0] = this._Rows;
-                s[1] = this._Cols;
-                return s;
+            {   
+                return this._Size;
             }
+        }
+
+        public Matrix Clone()
+        {
+            Matrix r = new Matrix(this.Size);
+            for (int i = 0; i < this.Length; i++)
+                r[i] = this[i];
+
+            return r;
         }
 
 
@@ -304,6 +519,30 @@ namespace GuiSonar2
 
             return m;
         }
+
+        public static implicit operator int[](Matrix m)  // Matrix to int[] conversion operator
+        {
+            if (m._Size.Length > 1)
+                throw new ArgumentException("Array dimensions must agree.");
+
+            int[] r = new int[m.Numel];
+            for (int i = 0; i < r.Length; i++)
+                r[i] = (int)m[i];
+
+            return r;
+        }
+
+
+        public static implicit operator Matrix (int[] f)  // Matrix to int[] conversion operator
+        {
+            Matrix m = new Matrix(f.Length, 1);  // explicit conversion
+
+            for (int i = 0; i < f.Length; i++)
+                m[i] = f[i];
+
+            return m;
+        }
+
 
         public static implicit operator Matrix(double f)  // double to Matrix conversion operator
         {
@@ -341,13 +580,14 @@ namespace GuiSonar2
             return m._Data;
         }
 
-        public Matrix Clone()
+        public override bool Equals(object obj)
         {
-            Matrix r = new Matrix(this.Size);
-            for (int i = 0; i < this.Length; i++)
-                r[i] = this[i];
+            return base.Equals(obj);
+        }
 
-            return r;
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
         }
     }
 }
